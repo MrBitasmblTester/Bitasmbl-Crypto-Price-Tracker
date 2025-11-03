bd7faa75517853d00a841a6c8a761d79687408c1
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-03 23:36:08 +0400

    Add CryptoTable.jsx

---
+++ src/components/CryptoTable.jsx
@@ -0,0 +1,2 @@
+// src/components/CryptoTable.jsx
+import React from 'react';\nexport default function CryptoTable({ data }) {\n  return (<table className="min-w-full">{/* map rows here */}</table>);\n}
